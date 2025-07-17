2cee08225e57feca82d6a97ea3a1646e5dc7ac02
proud king <3213626955@qq.com>
2025-07-17 21:39:53 +0800

    更新Mask_perform和任务MASK结构体，使任务可实时更新

--- Mask/Car.c
+++ Mask/Car.c
@@ -247,7 +247,8 @@ int Car_getnumfuc(CAR *car)
     static int pc = 0;
     if(pc == 0)
     {
-        K210_setnumstatus(1); //启动K210获取数字 标志位置位
+        K210_numstatusIN(1); //启动K210获取数字 标志位置位
+        K210_printf("{\"cmd\":\"num\"}\n");
         pc++;
         return 0; //开始获取
     }
@@ -440,7 +441,7 @@ MASK mask_c = {
      go_over,
      mask_load
     },
-    .mask_num = 20
+    .mask_num = 18
 };
 
 MASK mask_c1 = {
