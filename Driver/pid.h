f2ac6867bfeff2bbe0c471b31d40ea41ecd6ad86
proud king <3213626955@qq.com>
2025-07-19 15:14:46 +0800

    更新pid库

--- Driver/pid.h
+++ Driver/pid.h
@@ -5,10 +5,14 @@
 
 typedef struct
 {
+    double last_targetvalue; //上次目标值
+	double kff; //前馈系数
+	
 	double kp;
 	double ki;
 	double kd;
-	double last_bias;
+	double last_bias;  //上次偏差
+	double last2_bias; //上上次偏差
 	double integral;
 	double outvalue;
 	double last_outvalue;
@@ -25,7 +29,13 @@ double xianfu(double value, double min, double max);
 double position_divAPI_PID_Cal(double targetvalue,double currentvalue,double current_div,PID* pid);
 // 位置式 PID 计算函数（带输出限幅和积分抗饱和）
 double positionPid_Cal(double targetvalue, double currentvalue, PID* pid);
-double deltaPid_Cal(double targetvalue,double currentvalue,PID* pid);
+//位置式 FFPID 计算函数
+double positionFFPid_Cal(double targetvalue, double currentvalue, PID* pid);
+//增量式 PID 计算函数
+double deltaPid_Cal(double targetvalue, double currentvalue, PID* pid);
+//增量式 FFPID 计算函数
+double deltaFFPid_Cal(double targetvalue, double currentvalue, PID* pid);
+//清除PID内存
 void pidmemory_clear(PID* pid);
 
 #endif
