b4a9b3daa23ea59eb9e2c86ec7411cd46d338147
proud king <3213626955@qq.com>
2025-07-25 21:33:08 +0800

    更新Angle_pwm库 编译后对报错进行修改

--- Angle_pwm/Angle_config.c
+++ Angle_pwm/Angle_config.c
@@ -1,7 +1,7 @@
 #include "Angle_config.h"
 
 ANGLE_CONFIG angle_config = {
-    .htim = &htim1
+    .htim = &htim1, // Timer handle for input capture
     .prescaler = 8, // Default prescaler value, can be set later
     .cnt_clk = 72000000 / (8 + 1), // Counter clock frequency based on prescaler
 };
