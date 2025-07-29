7f4850d3bed0510eb5403e8846879ad494938114
proud king <3213626955@qq.com>
2025-07-29 20:13:51 +0800

    更细Aangle_config文件，增加安全性设置

--- Angle_pwm/Angle_config.c
+++ Angle_pwm/Angle_config.c
@@ -6,11 +6,15 @@ ANGLE_CONFIG angle1_config = {
     .htim = &htim1,
     .prescaler = 8, // Default prescaler value, can be set later
     .cnt_clk = 72000000 / (8 + 1), // Counter clock frequency based on prescaler
+
+    .angle_callbackfunc = NULL, // Initialize angle calculation callback to NULL
     .tim_ic_callback = angle_tim_ic_callback,
 };
 
 static void angle_tim_ic_callback(void) {
-   angle1_config.angle_callbackfunc(&angle1_config); // Call the angle calculation callback function
+  if(angle1_config.angle_callbackfunc != NULL) {
+    angle1_config.angle_callbackfunc(&angle1_config); // Call the angle calculation callback function
+  }
 }
 
 void Angle_config_set_anglecallback(ANGLE_CONFIG *config, void (*callback)(ANGLE_CONFIG *)) {
